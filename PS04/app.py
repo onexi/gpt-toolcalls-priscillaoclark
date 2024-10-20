c30a4fe825d0ae815eec86cde6527fb2f4ad10d1
Priscilla <17732979+priscillaoclark@users.noreply.github.com>
2024-10-20 03:38:27 +0000

    front end updates

--- PS04/app.py
+++ PS04/app.py
@@ -1,22 +1,19 @@
 from flask import Flask, render_template, request
 from main import main
-app = Flask(__name__)
 
-@app.route('/')
+app = Flask(__name__)  # Refers to self (the current file)
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        user_message = request.form['user_message']
+        try:
+            result = main(user_message)
+        except Exception as e:
+            app.logger.error(f"Error processing request: {str(e)}")
+            return "An error occurred. Please try again later.", 500
+        return render_template('result.html', result=result)
     return render_template('index.html')
-
-@app.route('/result', methods=['POST'])
-def result():
-    user_message = request.form['user_message']
-    #user_message = "When is the next train from South Station to Kendall Square?"
-    try:
-        result = main(user_message)
-    except Exception as e:
-        app.logger.error(f"Error processing request: {str(e)}")
-        return "An error occurred. Please try again later.", 500
-    return render_template('index.html', result=result)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
